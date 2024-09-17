87f4915b6a51bdcf9ecc7a0c7532aa2144fa6aac
danielpizarrotadres <danielpizarrodev@gmail.com>
2024-09-17 09:56:13 -0300

    :rotating_light: Linter warnings

--- main.py
+++ main.py
@@ -26,61 +26,57 @@ def find_audits(audits, criteria):
   return audits_found
 
 if __name__ == '__main__':
-    print('Start')
+  print('Start')
 
-    # Find all audits from 2024-09-14 until 2024-09-17
-    audit = Audit(db_url, db_name, db_collection)
-    since = datetime(2024, 9, 14, 0, 0, 0)
-    until = datetime(2024, 9, 17, 0, 0, 0)
-    print(f"Criteria: {since} -> {until}")
-    audits = audit.find({
-        "createdAt": {
-            "$gte": since,
-            "$lte": until
-        }
+  # Find all audits from 2024-09-14 until 2024-09-17
+  audit = Audit(db_url, db_name, db_collection)
+  since = datetime(2024, 9, 12, 0, 0, 0)
+  until = datetime(2024, 9, 17, 0, 0, 0)
+  print(f"Criteria: {since} -> {until}")
+  audits = audit.find({
+    "createdAt": {
+      "$gte": since,
+      "$lte": until
+      }
     })
-    print(f"Total audits: {len(audits)}")
+  print(f"Total audits: {len(audits)}")
 
-    # Find all audits from PDC database
-    pdc = Pdc()
-    audits_from_pdc = pdc.find()
+  # Find all audits from PDC database
+  pdc = Pdc()
+  audits_from_pdc = pdc.find()
 
-    # List to collect all data
-    all_data = []
+  # List to collect all data
+  all_data = []
 
-    # Iterate over each audit from PDC database and find it in the audits
-    for index, item in enumerate(audits_from_pdc):
-        print(f'Processing audit {index + 1} of {len(audits_from_pdc)}')
+  # Iterate over each audit from PDC database and find it in the audits
+  for index, item in enumerate(audits_from_pdc):
+    print(f'Processing audit {index + 1} of {len(audits_from_pdc)}')
 
-        audits_belonging_pdc = find_audits(audits, item)
-        print(f"Total audits_belonging_pdc found: {len(audits_belonging_pdc)}")
+    audits_belonging_pdc = find_audits(audits, item)
+    print(f"Total audits_belonging_pdc found: {len(audits_belonging_pdc)}")
 
-        if len(audits_belonging_pdc) == 0:
-            print(f"Audit not found in the database")
-            continue
+    if len(audits_belonging_pdc) == 0:
+      print(f"Audit not found in the database")
+      continue
 
-        for j, element in enumerate(audits_belonging_pdc):
-            print(f'Processing audit_belonging_pdc {j + 1} of {len(audits_belonging_pdc)}')
+    for j, element in enumerate(audits_belonging_pdc):
+      print(f'Processing audit_belonging_pdc {j + 1} of {len(audits_belonging_pdc)}')
 
-            all_data.append({
-                'PDC EMPLOYEE': item.get('EMPLOYEE'),
-                'PDC TSTAMP': item.get('TSTAMP'),
-                'PDC ID': item.get('ID'),
-                'PDC IDDATE': item.get('IDDATE'),
-                'AUDIT ORIGIN': element.get('origin'),
-                'AUDIT DESTINATION': element.get('destination'),
-                'AUDIT FLIGHT ID': element.get('flightId'),
-                'AUDIT DEPARTURE DATE': element.get('departureDate'),
-                'AUDIT ID': element.get('_id'), 
-            })
+      all_data.append({
+        '[PDC] EMPLOYEE': item.get('EMPLOYEE'),
+        '[PDC] TSTAMP': item.get('TSTAMP'),
+        '[PDC] ID': item.get('ID'),
+        '[PDC] IDDATE': item.get('IDDATE'),
+        '[PDC] ACTION': item.get('ACTION'),
+        '[AUDIT] ORIGIN': element.get('origin'),
+        '[AUDIT] DESTINATION': element.get('destination'),
+        '[AUDIT] FLIGHT ID': element.get('flightId'),
+        '[AUDIT] DEPARTURE DATE': element.get('departureDate'),
+        '[AUDIT] ID': element.get('_id'),
+      })
 
-    # Create a DataFrame from the collected data
-    df = pd.DataFrame(all_data)
-
-    # Get the current date and time
-    current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
-    file_name = f'report_{current_time}.xlsx'
-
-    # Save the DataFrame to an Excel file
-    df.to_excel(file_name, index=False)
-    print(f'Excel file {file_name} created successfully')
+  df = pd.DataFrame(all_data)
+  current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+  file_name = f'report_{current_time}.xlsx'
+  df.to_excel(file_name, index=False)
+  print(f'Excel file {file_name} created successfully')
